c4ff1a32d57e8bde9713bf2a56c9302c43457b78
4tunea <architektas3@gmail.com>
2026-03-15 00:06:10 +0200

    improved code

--- BronzeLeague/main.cpp
+++ BronzeLeague/main.cpp
@@ -75,37 +75,66 @@ void PRINT(const vector<string> & map){
 //  --------------  DEBUG END
 
 bool isFalling(const Game & game, const vector<string> & fullMap, const vector<Coord> & b){
-    bool falling {true};
     for(auto i : b){
         if(i.y + 1 >= game.height) continue;
-        if(fullMap[i.y + 1][i.x] != '.'){
-            falling = false;
+        if(fullMap[i.y + 1][i.x] != '.') return false; 
+    }
+    return true;
+}
+
+void doGravityWork(const Game & game, const vector<string> & mapFull, vector<Coord> & b){
+    int minFall {INT_MAX};
+    map<int, int> checkList;
+    for(size_t i {}; i < b.size(); ++i){
+        if(checkList[b[i].x] == 0){
+            checkList[b[i].x] = b[i].y + 1;
+        } else {
+            if(checkList[b[i].x] < b[i].y + 1){
+                checkList[b[i].x] = b[i].y + 1;
+            }
         }
     }
-    /*   DEBUG
-    if(falling){cerr << "\nFALLING: ";
-        for(auto j : b){
-                cerr << j.x << ":" << j.y << " | ";
+
+    for(auto i : checkList){
+        int y {i.second};
+        while(y < game.height){
+            if(mapFull[y][i.first] != '.'){
+                if(minFall > y - i.second){
+                    minFall = y - i.second;
+                }
+                break;
+            }
+            y += 1;
         }
-        cerr << '\n';
     }
-    */
-    return falling;
+
+    for(auto i : b){
+        cerr << "|" <<i.x << "," << i.y<<"|";
+    }
+    cerr<<"\n"<<minFall<<"\n";
+
+    for(auto & i : b){
+        i.y += minFall;
+    }
 }
 
 vector<Coord> bodyMove(const Game & game, const vector<string> & map, const vector<Coord> & b, Coord d, bool tookPower){
     vector<Coord> body {b};
     if(tookPower) body.push_back(body[body.size()-1]);
-    for(size_t i {}; i < b.size(); ++i){
-        if(i == 0){
-            body[i] = {b[i].x + d.x, b[i].y + d.y};
-            continue;
+    body[0] = {b[0].x + d.x, b[0].y + d.y};
+    for(size_t i {1}; i < body.size(); ++i){
+        if(i-1 < b.size()) {
+            body[i] = {b[i-1].x, b[i-1].y};
+        } else {
+            body[i] = body[i-1];
         }
-        body[i] = {b[i-1].x, b[i-1].y};
-    } 
-    if(isFalling(game, map, body)){
-        
     }
+
+    if(isFalling(game, map, body)){
+        doGravityWork(game, map, body);
+    }
+    
+    for(auto i : body){cerr << "|" <<i.x << "," << i.y<<"|";}cerr << "\n\n";
     return body;
 }
 
@@ -144,28 +173,36 @@ string simpleBfs(const Game & game, const vector<string> & map, const vector<str
             continue;
         }
 
-        Coord dir[4] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        Coord dir[4] {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        if(q.snake.id % 2 == 0){
+            Coord temp = dir[0];
+            dir[0] = dir[1];
+            dir[1] = dir[2];
+            dir[2] = dir[3];
+            dir[3] = temp;
+        }
+        
         for(auto d : dir){
             bool valid {1};
-            if(q.snake.h.x + d.x == game.width || q.snake.h.x + d.x == -1 || q.snake.h.y + d.y == game.height || q.snake.h.y + d.y == -1) valid = 0;
-            if(map[q.snake.h.y + d.y][q.snake.h.x + d.x] == '#' || map[q.snake.h.y + d.y][q.snake.h.x + d.x] == '0') valid = 0;
+            if(q.snake.h.x + d.x >= game.width || q.snake.h.x + d.x < 0 || q.snake.h.y + d.y >= game.height || q.snake.h.y + d.y < 0) continue;
+            if(map[q.snake.h.y + d.y][q.snake.h.x + d.x] == '#' || map[q.snake.h.y + d.y][q.snake.h.x + d.x] == '0') continue;
             for(size_t i {}; i < q.snake.b.size() - 1; ++i){   //  -1 because skip last body part (it moves)
                 if(q.snake.b[i].x == q.snake.h.x + d.x && q.snake.b[i].y == q.snake.h.y + d.y){
                     valid = 0;
                 }
             }
-            if(q.snake.h.x + d.x == q.snake.b[1].x && q.snake.h.y + d.y == q.snake.b[1].y) valid = 0;
+            if(q.snake.h.x + d.x == q.snake.b[1].x && q.snake.h.y + d.y == q.snake.b[1].y) continue;
             if(!valid) continue;
+            double tempPw {q.points + 0.01};  // move points
             Snake tempSn {q.snake};
-            double tempPw {q.points - 0.01};  // move penalty
             bool tookPower {};
             if(map[q.snake.h.y + d.y][q.snake.h.x + d.x] == 'P'){
-                tempPw += 1;
+                tempPw += 5 - (0.1 * q.depth);
                 tookPower = true;
             }
-            tempSn.h.y += d.y;
-            tempSn.h.x += d.x;
             tempSn.b = bodyMove(game, fullMap, q.snake.b, d, tookPower);
+            tempSn.h.y = tempSn.b[0].y;
+            tempSn.h.x = tempSn.b[0].x;
             if(q.origin.x == -1){
                 que.push({tempSn, tempPw, tempSn.h, q.depth + 1});
             } else {
@@ -242,6 +279,7 @@ int main()
         }
 
         //  ----------  MAIN LOGIC
+        vector<Coord> nextMoves {};
         for(auto & i : snake){
             if(!i.mySnake) continue;
             vector<string> mapSn {map};
@@ -249,13 +287,31 @@ int main()
             for(auto j : snake){
                 if(i.id == j.id) continue;
                 for(size_t z {}; z < j.b.size(); ++z){
+                    if(j.b[z].x >= game.width || j.b[z].x < 0 || j.b[z].y >= game.height || j.b[z].y < 0) continue;
                     if(z != j.b.size() - 1) mapSn[j.b[z].y][j.b[z].x] = '0';
                     fullMap[j.b[z].y][j.b[z].x] = '0';
                 }
             }
-            PRINT(mapSn);
-            PRINT(fullMap);
-            i.nextMove = simpleBfs(game, mapSn, fullMap, i, 3);
+            for(auto i : nextMoves){
+                if(i.x >= game.width || i.x < 0 || i.y >= game.height || i.y < 0) continue;
+                mapSn[i.y][i.x] = '0';
+                fullMap[i.y][i.x] = '0';
+            }
+
+            i.nextMove = simpleBfs(game, mapSn, fullMap, i, 5);
+            
+            if(i.nextMove == "UP"){
+                nextMoves.push_back({i.h.x, i.h.y - 1});
+            }
+            if(i.nextMove == "DOWN"){
+                nextMoves.push_back({i.h.x, i.h.y + 1});
+            }
+            if(i.nextMove == "LEFT"){
+                nextMoves.push_back({i.h.x - 1, i.h.y});
+            }
+            if(i.nextMove == "RIGHT"){
+                nextMoves.push_back({i.h.x + 1, i.h.y});
+            }
         }
         
         string command {};
@@ -268,5 +324,7 @@ int main()
             }
         }
         cout << command << endl;
+
+        PRINT(snake);
     }
 }
